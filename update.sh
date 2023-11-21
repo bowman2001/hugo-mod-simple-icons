09f78b5fdedad486392d6ee2b42d03c9a645ca05
Georg Makowski <gm@desider.at>
2023-11-21 10:38:22 +0100

    chore: update icons

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -av node_modules/simple-icons/icons/*.svg assets/simple-icons
+cp node_modules/simple-icons/icons/*.svg assets/svg/simple-icons
 cp node_modules/simple-icons/_data/simple-icons.json data/simple_icons/simple_icons.json
