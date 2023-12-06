f93aa1cf22c1d3f8ac7b023500e5db1038bc2256
Georg Makowski <gm@desider.at>
2023-12-06 11:44:22 +0100

    update to 10.1.0

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cp node_modules/simple-icons/icons/*.svg assets/svg/simple-icons
-cp node_modules/simple-icons/_data/simple-icons.json data/simple_icons/simple_icons.json
+rsync -a --delete node_modules/simple-icons/icons/ assets/svg/simple-icons
+cp -u node_modules/simple-icons/_data/simple-icons.json data/simpleIcons/simpleIcons.json
